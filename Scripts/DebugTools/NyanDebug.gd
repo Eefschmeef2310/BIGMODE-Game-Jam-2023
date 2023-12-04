2a8e121c142ad5f5d2060afc35e024e20ba9cf37
Clevertop <41929769+Clevertop@users.noreply.github.com>
2023-12-05 00:24:02 +1100

    new commands setup with emmiters

--- Scripts/DebugTools/NyanDebug.gd
+++ Scripts/DebugTools/NyanDebug.gd
@@ -5,11 +5,16 @@
 
 extends Node
 
-var active = false
+var active = false as bool
 @onready var inputBox : LineEdit = $CanvasLayer/LineEdit
 @onready var outputBox : TextEdit =  $CanvasLayer/TextEdit
-@export var freeCamNode : Camera2D
-@export var gameCamNode : Camera2D
+@export var freeCam : Camera2D
+@export var gameCam : Camera2D
+
+signal cmdFreecam
+signal cmdGamecam
+signal cmdStonks
+signal cmdGodmode
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,24 +31,40 @@ func _process(delta):
 
 
 func _on_line_edit_text_submitted(new_text):
-	var command = new_text
+	var command = new_text as String
+	command = command.to_lower()
 	inputBox.clear()
 	match command:
 		"help":
+			# standard "help" command
 			meow("use 'nya' to view the list of commands")
 		"nya":
+			# actual help command
 			meow("Available commands:")
 			meow("help, nya, freecam, gamecam")
+		"meow":
+			# nya nya~!
+			meow("the console meows back happily :3")
 		"freecam":
-			freeCamNode.make_current()
-			freeCamNode.process_mode = Node.PROCESS_MODE_INHERIT
+			#enter freecam mode 
+			freeCam.make_current()
+			freeCam.process_mode = Node.PROCESS_MODE_INHERIT
 			meow("entered freecam mode")
 			meow("use WASD to pan, Q to zoom out and E to zoom in")
+			cmdFreecam.emit()
 		"gamecam":
-			gameCamNode.make_current()
-			freeCamNode.process_mode = Node.PROCESS_MODE_DISABLED
+			#return camera to normal mode
+			gameCam.make_current()
+			freeCam.process_mode = Node.PROCESS_MODE_DISABLED
 			meow("switched to normal camera")
-		_:
+			cmdGamecam.emit()
+		"stonks":
+			#infinite gears
+			cmdStonks.emit()
+		"godmode":
+			# Life, it never die
+			cmdGodmode.emit()
+		_: #wildcard
 			meow("command: '" + command + "' not found" )
 		
 
