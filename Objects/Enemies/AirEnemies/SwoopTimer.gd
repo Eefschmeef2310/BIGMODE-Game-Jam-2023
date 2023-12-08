95cb94a7bf4cc66c54c28cc5dc074026300cb83b
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2023-12-08 11:24:07 +1100

    Swooping enemy

---
+++ Objects/Enemies/AirEnemies/SwoopTimer.gd
@@ -0,0 +1,19 @@
+extends Timer
+
+var initialY
+var targetY
+var tween
+
+var time = 1
+
+func _on_timeout():
+	initialY = $"..".position.y
+	targetY = GameManager.tank_position.y - 10
+	tween = create_tween()
+
+	tween.tween_property($"..", "position:y", targetY, time).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	tween.tween_property($"..", "position:y", initialY, time).set_ease(Tween.EASE_IN)
+	tween.tween_callback(restart)
+	
+func restart():
+	start()
