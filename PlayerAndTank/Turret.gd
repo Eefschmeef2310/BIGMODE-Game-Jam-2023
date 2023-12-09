a4701c180e70aa7beaee85a75a3ae6990abd8b38
Eefschmeef2310 <ethan.lucasawesome@gmail.com>
2023-12-10 00:15:31 +1100

    Added guide arrow

--- PlayerAndTank/Turret.gd
+++ PlayerAndTank/Turret.gd
@@ -4,12 +4,18 @@ extends StaticBody2D
 
 func _process(delta):
 	if GameManager.tank_mode:
-		var angle = (get_global_mouse_position() - global_position).normalized().angle()
+		var angle = (get_global_mouse_position() - global_position).angle()
+		
+		if angle > 0:
+			if angle > PI/2 and angle < (11*PI)/12:
+				angle = (11*PI)/12
+			elif angle < PI/2 and angle > PI/12:
+				angle = PI/12
 		
 		#if angle > 0:
 			#if angle > PI/2:
 				#angle = -PI
 			#elif angle < PI/2:
 				#angle = 0
-		angle = clamp(angle, -(3*PI)/4, PI/4)
-		rotation = lerp(rotation, angle, rotation_speed * delta)
+		#angle = clamp(angle, -(3*PI)/4, PI/4)
+		rotation = rotate_toward(rotation, angle, rotation_speed * delta)
